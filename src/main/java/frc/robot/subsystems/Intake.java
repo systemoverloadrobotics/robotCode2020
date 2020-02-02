5c54c0544de2144b2bf7635004da650633124d90
Michael Crandall <michaelcrandall33@gmail.com>
2020-02-01 18:25:05 -0800

    added shoot function

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
@@ -8,7 +9,7 @@ public class Intake extends SubsystemBase {
     WPI_VictorSPX test = new WPI_VictorSPX(1);
 
     public Intake(){
-
+        test.set(ControlMode.PercentOutput, 3);
 
     }
 
