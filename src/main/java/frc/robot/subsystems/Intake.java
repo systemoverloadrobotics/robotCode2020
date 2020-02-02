3ee07d0a4cf6c560b2d00bb737f50d845139e32e
Jason Hsu <jason@nyentek.com>
2020-02-01 18:33:34 -0800

    annoy crandall

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
+    public static final boolean MakeCrandallWorkAgain = true;
+
     public Intake(){
 
 
